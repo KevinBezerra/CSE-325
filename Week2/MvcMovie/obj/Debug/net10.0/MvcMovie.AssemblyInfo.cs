47f6f7cce06948d94d67ccee994223912892d92f
KevinBezerra <kevinmbezerra@gmail.com>
2026-07-11 14:50:54 -0300

    W02 Assignment: ASP.NET Core - 2

--- Week2/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ Week2/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb5c99282e1f06f46be90f5eb39a9c320652739d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2eb1e39608f3e1393536ecd05dc3201d7964ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
